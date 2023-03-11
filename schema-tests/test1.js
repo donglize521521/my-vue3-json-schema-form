44da4498c343792d7f90adaa6cfb53156b680f9d
星霜下逆天生 <1486252015@qq.com>
2023-03-12 00:23:18 +0800

    feat(3-3和3-4): 1.format 2.自定义format 3.自定义关键字

--- schema-tests/test1.js
+++ schema-tests/test1.js
@@ -1,23 +1,68 @@
-const Ajv = require("ajv")
-const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+const Ajv = require('ajv') //
+const ajv = new Ajv()
 
+// 一、自定义 Format
+ajv.addFormat('test', (data) => {
+  console.log(data, '--------')
+  return data === 'hahaha'
+})
+// 二、自定义关键字
+ajv.addKeyword('testKeyword', {
+// 方式 1 validate
+//   validate(schema, data) {
+//     console.log(schema, data)
+//     console.log(schema.length)
+//     if (schema === true) return true
+//     else return schema.length === 6
+//   },
+// 方式2 
+// compile(sch, parentSchema) {
+//     console.log(sch, parentSchema)
+//     return () => true
+//   },
+//   metaSchema: {
+//     type: 'boolean',
+//   },
+// 方式3 会校验关键字所在的属性 (推荐使用)
+//   macro() {
+//     return {
+//       minLength: 10,
+//     }
+//   },
+// 方式4 inline(性能最好,阅读性不好)
+
+})
 const schema = {
-    type: "object",
-    properties: {
-      foo: {type: "integer"},
-      bar: {type: "string"}
+  type: 'object',
+  properties: {
+    name: {
+      type: 'string',
+      // format: 'test',
+      testKeyword: true,
     },
-    required: ["foo"],
-    additionalProperties: false
-  }
-  
-  const validate = ajv.compile(schema)
-  
-  const data = {
-    foo: 1,
-    bar: "abc"
-  }
-  
-  const valid = validate(data)
-  
-  if (!valid) console.log(validate.errors)
+    age: {
+      type: 'number',
+    },
+    pets: {
+      type: 'array',
+      items: {
+        type: 'string',
+      },
+    },
+    isWorker: {
+      type: 'boolean',
+    },
+  },
+  required: ['name', 'age'], //哪些属性是必须输入的
+}
+
+const validate = ajv.compile(schema)
+
+const valid = validate({
+  name: 'hahaha',
+  age: 10,
+  pets: [],
+  isWorker: false,
+})
+if (!valid) console.log(validate, validate.errors)
+
