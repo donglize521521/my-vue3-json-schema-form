d3cae431939fa5347bf965fe0b2855f261db8e7b
星霜下逆天生 <1486252015@qq.com>
2023-03-12 18:59:38 +0800

    feat(第3章): JsonSchema标准和使用方式介绍

--- schema-tests/test1.js
+++ schema-tests/test1.js
@@ -1,11 +1,12 @@
 const Ajv = require('ajv') //
-const ajv = new Ajv()
+const ajv = new Ajv({allErrors:true,jsonPointers:true});
+const localize = require('ajv-i18n')
 
 // 一、自定义 Format
-ajv.addFormat('test', (data) => {
-  console.log(data, '--------')
-  return data === 'hahaha'
-})
+// ajv.addFormat('test', (data) => {
+//   console.log(data, '--------')
+//   return data === 'hahah'
+// })
 // 二、自定义关键字
 ajv.addKeyword('testKeyword', {
 // 方式 1 validate
@@ -24,21 +25,28 @@ ajv.addKeyword('testKeyword', {
 //     type: 'boolean',
 //   },
 // 方式3 会校验关键字所在的属性 (推荐使用)
-//   macro() {
-//     return {
-//       minLength: 10,
-//     }
-//   },
+  macro() {
+    return {
+      minLength: 10,
+    }
+  },
 // 方式4 inline(性能最好,阅读性不好)
 
+
 })
+require('ajv-errors')(ajv)
 const schema = {
   type: 'object',
   properties: {
     name: {
       type: 'string',
-      // format: 'test',
-      testKeyword: true,
+    //   format: 'test',
+      testKeyword: false,
+      minLength: 10,
+      errorMessage: {
+        type: '必须是字符串',
+        minLength: '长度不能小于 10',
+      },
     },
     age: {
       type: 'number',
@@ -64,5 +72,9 @@ const valid = validate({
   pets: [],
   isWorker: false,
 })
-if (!valid) console.log(validate, validate.errors)
+if (!valid) {
+    // 返回中文错误信息
+//   localize.zh(validate.errors)
+  console.log(validate, validate.errors)
+}
 
