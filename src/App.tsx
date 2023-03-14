457da22f7d053e2f89677e2794dc5511813f39fe
星霜下逆天生 <1486252015@qq.com>
2023-03-14 09:09:58 +0800

    feat(4.2): 初步搭建demo项目

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { defineComponent, Ref, ref } from 'vue'
+import { createUseStyles } from 'vue-jss'
+
+import MonacoEditor from './components/MonacoEditor'
+
+function toJson(data: any) {
+  return JSON.stringify(data, null, 2)
+}
+
+const schema = {
+  type: 'string',
+}
+
+const useStyles = createUseStyles({
+  editor: {
+    minHeight: 400,
+  },
+})
+
+export default defineComponent({
+  setup() {
+    const schemaRef: Ref<any> = ref(schema)
+    const handleCodeChange = (code: string) => {
+      let schema: any
+      try {
+        schema = JSON.parse(code)
+      } catch (err) {
+        schemaRef.value = schema
+      }
+    }
+    const classesRef = useStyles()
+    return () => {
+      const classes = classesRef.value
+      const code = toJson(schemaRef.value)
+      return (
+        <div>
+          <MonacoEditor
+            code={code}
+            onChange={handleCodeChange}
+            title="Schema"
+            class={classes.editor}
+          />
+        </div>
+      )
+    }
+  },
+})
+// yarn add monaco-editor -D
+
+//yarn add vue-jss -S
+// yarn add jss-preset-default -S
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,4 @@
-import { createApp, defineComponent, h } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-// import App from './App.vue'
-const img = require('./assets/logo.png')//eslint-disable-line
-const App = defineComponent({
-  render() {
-    return h('div', { id: 'app' }, [
-      h('img', {
-        alt: 'Vue logo',
-        src: img,
-      }),
-      h(HelloWorld, {
-        msg: 'Welcome to Your Vue.js + TypeScript App',
-        age: 12,
-      }),
-    ])
-  },
-})
+import { createApp } from 'vue'
+import App from './App'
 
 createApp(App).mount('#app')
